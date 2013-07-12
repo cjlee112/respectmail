d0349b7bd725f8003325f151badd21b9415fdd4e
Christopher Lee <leec@chem.ucla.edu>
2013-07-12 13:28:57 -0700

    make triage look for config in current dir automatically

--- respectmail/triage.py
+++ respectmail/triage.py
@@ -2,7 +2,14 @@ import db
 import imap
 
 def do_triage():
-    import config
+    try:
+        import config
+    except ImportError:
+        import sys
+        import os
+        sys.path.append(os.getcwd()) # look for config in current dir
+        import config
+
     servers = config.mailServers
     triageDB = db.TriageDB()
     for s in servers:
